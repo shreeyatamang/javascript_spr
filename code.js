42482045c57b4865d4b45eb3d01d6bec47ebe815
shreeya tamang <121777681+shreeyatamang@users.noreply.github.com>
2023-07-02 20:43:01 +0545

    Add files via upload

---
+++ code.js
@@ -0,0 +1,21 @@
+console.log('Hi everyone this is shreeya')
+console.log('my favourite player is cristiano')
+console.log('cr.7')
+x = 5;
+y = 6;
+z = x + y;
+console.log(z)
+let a = 6;
+let b = 3;
+let c = a + b;
+console.log(c)
+const p = 5;
+const q = 3;
+const r = p + q;
+console.log(r)
+let person = "shreeya",
+    carname = "volvo",
+    price = 200;
+console.log(person);
+console.log(carname);
+console.log(price);
